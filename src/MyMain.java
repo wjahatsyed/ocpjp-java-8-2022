297371b79e1b6cb1fad36eebf6e1b78038007447
syedwajahat1 <syed.wajahat@backcountry.com>
2022-09-19 17:04:29 +0500

    First commit!

---
+++ src/MyMain.java
@@ -0,0 +1,9 @@
+/**
+ * @author Syed Muhammad Wajahat
+ * Creation date: 19/09/2022
+ */
+public class MyMain {
+    public static void main(String[] args){
+        System.out.println("Welcome! This ");
+    }
+}
